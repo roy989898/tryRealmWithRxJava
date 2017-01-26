8e9f454f0d07fca252aa2582c9056951b9707d41
roy.leung <Roy.Leung@RoyLeungs-MacBook-Air.local>
2017-01-26 11:17:10 +0800

    create layout

--- app/src/main/java/pom/realmwithrxhava/MainActivity.java
+++ app/src/main/java/pom/realmwithrxhava/MainActivity.java
@@ -1,13 +1,31 @@
 package pom.realmwithrxhava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ed_name)
+    EditText edName;
+    @BindView(R.id.ed_old)
+    EditText edOld;
+    @BindView(R.id.bt_save)
+    Button btSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bt_save)
+    public void onClick() {
     }
 }
